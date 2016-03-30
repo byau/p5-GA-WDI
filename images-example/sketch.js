d093c729b0a3ae4892fbe144ef9a315969d6f1eb
byau <billy.l.yau@gmail.com>
2016-03-30 11:23:17 -0400

    updated img example

--- images-example/sketch.js
+++ images-example/sketch.js
@@ -1,3 +1,7 @@
+var x = 100;
+var y = 100;
+var xsteps=1;
+var ysteps=1;
 var img;
 function preload(){
   img = loadImage('/images/Emoji/Smiley/Emoji_Smiley-90.png');
@@ -6,10 +10,27 @@ function preload(){
 
 function setup() {
   createCanvas(screen.availWidth, screen.availHeight);
-// background(250, 250, 100);
 }
 
 function draw() {
   background(250, 250, 100);
-  image(img, mouseX, mouseY);
+  image(img, x, y);
+  x += xsteps;
+  y += ysteps;
+  if(x > windowWidth - img.width){
+    xsteps = -1;
+  }
+  if(x < 0){
+    xsteps = +1;
+  }
+  if(y > windowHeight - img.height){
+    ysteps = -1;
+  }
+  if(y < 0){
+    ysteps = +1;
+  }
 }
+
+// function mousePressed() {
+//   noLoop();
+// }
